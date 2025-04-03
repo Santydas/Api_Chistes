1eda84fda667318501827876e06bd32f3b2abfb0
Santydas <dsdazam@academia.usbbog.edu.co>
2025-04-03 00:22:30 -0500

    Add files via upload

---
+++ ChistesYFrases/src/main/java/com/example/chistesyfrases/Controllers/ChistesYFrasesController.java
@@ -0,0 +1,79 @@
+package com.example.chistesyfrases.Controllers;
+
+import com.example.chistesyfrases.Entities.ChistesYFrasesEntity;
+import com.example.chistesyfrases.Services.ChistesYFrasesService;
+
+import jakarta.validation.Valid;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/chistesyfrases")
+@Validated
+public class ChistesYFrasesController {
+
+    private final ChistesYFrasesService chistesYFrasesService;
+
+    @Autowired
+    public ChistesYFrasesController(ChistesYFrasesService chistesYFrasesService) {
+        this.chistesYFrasesService = chistesYFrasesService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllchistesYFrases(
+    @RequestParam(defaultValue = "0") int page,
+    @RequestParam(defaultValue = "5") int size,
+    @RequestParam(defaultValue = "ChisteOFrase,asc") String[] sort){
+        try{
+            Pageable pageable = PageRequest.of(page, size, Sort.by(parseSort(sort)));
+            return chistesYFrasesService.getAllchistesYFrases(pageable);
+        }catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Orden incorrecto usa 'asc' o 'desc'.");
+        }
+    }
+
+    private Sort.Direction parseSort(String[] sort) {
+        if (sort.length < 2) {
+            throw new IllegalArgumentException("El parámetro de ordenación debe tener tanto campo como dirección");
+        }
+
+        String property = sort[0];
+        String direction = sort[1].toLowerCase();
+
+        List<String> validDirections = Arrays.asList("asc ", "desc");
+        if (!validDirections.contains(direction)) {
+            throw new IllegalArgumentException("Orden incorrecto usa 'asc' o 'desc'.");
+        }
+        return new Sort.Order(Sort.Direction.fromString(direction), property).getDirection();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getChistesYFrasesById(@PathVariable UUID id){
+        return chistesYFrasesService.getChistesYFrasesById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insertChiste(@Valid @RequestBody ChistesYFrasesEntity chistesYFrasesEntity){
+        return chistesYFrasesService.addChiste(chistesYFrasesEntity);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateChiste(@PathVariable UUID id, @Valid @RequestBody ChistesYFrasesEntity chistesYFrasesEntity){
+        return chistesYFrasesService.updateChiste(id, chistesYFrasesEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteChiste(@PathVariable UUID id){
+        return chistesYFrasesService.deleteChiste(id);
+    }
+
+}
